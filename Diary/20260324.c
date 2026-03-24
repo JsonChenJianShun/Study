64e9a6ed1d34a747334d3adf54e51c877bb1cba5
ChenJianShun <JsonChenJianShun@outlook.com>
2026-03-24 16:28:49 +0800

    add diary 20260324

---
+++ Diary/20260324.c
@@ -0,0 +1,128 @@
+print{
+ deg_Printf("JSON: line: %d, func: %s\n",__LINE__,__FUNCTION__);
+ hx_printk("JSON: line: %d, func: %s\n",__LINE__,__FUNCTION__);
+	 
+//JSON-----------------------START:
+
+//JSON-------------------------END
+
+ //JSON-TIPS：某些客户不需要升级后自动格式化功能
+}
+
+Avora{ 
+OR1K_GCC_PATH = r'D:\ChenJianShun\02_software\24_toolchain\hx330x-gcc-elf-newlib-sf-mingw-V4.9.1\bin'
+RISCV_GCC_PATH = r'D:\ChenJianShun\02_software\24_toolchain\Xuantie-900-gcc-elf-newlib-mingw-V3.0.1\bin'
+}
+
+读盘模式{
+	u32 usb_mode;
+	res_image_show(R_ID_IMAGE_PCCAM_MODE, 1);
+	usb_mode = USB_DEVTYPE_COMBINE;
+	usbDeviceOp.ui_show_state = 0;
+	task_com_sound_wait_end();
+
+	dusb_api_Init(usb_mode);
+	usbDeviceOp.usb_process_flag = 1;
+}
+
+other{
+	git reset --soft HEAD^
+	Cjs416@JSON
+}
+
+项目{
+	Y87,3508E
+	504A1,3508C
+	H0892,3508C
+	X5005,3506F
+	H0694,3506F
+	8405，3506F
+}
+
+鸿芯群问题{
+	1、检测TE，超时复位屏幕（HX330x_SDK_2M_TE_timeout_test）；
+}
+
+电脑重启{
+	20260310:重启电脑；
+	20260312:电脑卡顿；
+	20260312-1119:重启电脑；
+	20260313-1600:电脑卡顿；
+}
+
+金子{
+	1141.24 * 0.1 = 114.124;
+	1141.24 - 114.124 = 1027.116;
+}
+
+待购买的东西{
+	1、两双运动鞋；
+	2、向日葵远程开机设备（分享）；
+}
+
+目标{
+	1、每小时喝一次水，每次喝100ml；
+	2、每日早上重启一次电脑；
+}
+
+喝水{
+	20260313-1617:50ml；
+			 1636:25ml；
+			 1657:25ml；
+			 1734:25ml;200ml;
+			 1738:厕所；
+			 1749:25ml；150ml；
+}
+
+
+
+自己{
+	踩空了，
+}
+
+词条{
+video
+take a photo
+playback
+Settings
+}
+
+x5005图片{
+	956,132，143%
+}
+
+H0694问题{
+	1、录像变快：复现5次；
+	2、小镜头卡：复现3次；
+	3、录像重录：复现15次；
+	4、点不动：复现3次；
+	5、录像卡着不动：复现7次；
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
