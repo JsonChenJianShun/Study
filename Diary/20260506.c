080aba266806188b45bf876f8daa1d567a52e4ec
ChenJianShun <JsonChenJianShun@outlook.com>
2026-05-06 09:35:35 +0800

    添加diary 20260506

---
+++ Diary/20260506.c
@@ -0,0 +1,104 @@
+print{
+ deg_Printf("JSON: line: %d, func: %s\n",__LINE__,__FUNCTION__);
+ hx_printk("JSON: line: %d, func: %s\n",__LINE__,__FUNCTION__);
+	 
+//JSON-----------------------START:
+
+//JSON-------------------------END
+
+ //JSON-TIPS：某些客户不需要升级后自动格式化功能
+}
+
+Avora{ 
+OR1K_GCC_PATH = r'D:\ChenJianShun\02_software\24_toolchain\hx330x-gcc-elf-newlib-sf-mingw-V4.9.1\bin'
+XUANTIE_900_GCC_PATH = r'D:\ChenJianShun\02_software\24_toolchain\Xuantie-900-gcc-elf-newlib-mingw-V3.0.1\bin'
+}
+
+读盘模式{
+	u32 usb_mode;
+	res_image_show(R_ID_IMAGE_PCCAM_MODE, 1);
+	usb_mode = USB_DEVTYPE_COMBINE;
+	usbDeviceOp.ui_show_state = 0;
+	task_com_sound_wait_end();
+
+	dusb_api_Init(usb_mode);
+	usbDeviceOp.usb_process_flag = 1;
+}
+
+other{
+	git reset --soft HEAD^
+	Cjs416@JSON
+}
+
+项目{	
+	20260424{
+		1,DC228C;
+		2,0204;
+		3,H0634;
+		4,H69;
+		6,y17;
+		
+		y17,
+	}
+	
+	20260425{
+		1,DC228C;
+		2,0204;
+		3,H0634;
+		4,H0694;
+		5,H0892;
+		6,X5005;
+		
+		X5005,DC228C,H0892;
+	}
+}
+
+金子{
+	1141.24 * 0.1 = 114.124;
+	1141.24 - 114.124 = 1027.116;
+}
+
+待购买的东西{
+	1、两双运动鞋；
+	2、远程开机设备（分享）；
+	3、裤子；
+}
+
+
+
+DC228C颜色{
+	蓝色：30,144,255，1e90ff
+	黄色：255,224,0,ffe000
+}
+
+菜单项目{
+	曝光；
+	语言；
+	日期；
+	自动关机；
+	光源频率；
+	音量开关；
+	格式化；
+	默认设置；
+	
+	50,85,120,155,190,225,260,295,
+}
+
+DC228弹窗{
+	客户需要跟照片一比一的弹窗；
+	主要问题是
+	1、圆角的弹窗；
+	2、中间透明的区域；
+	3、可移动的上下滑块；
+}
+
+
+
+
+
+
+
+
+
+
+
